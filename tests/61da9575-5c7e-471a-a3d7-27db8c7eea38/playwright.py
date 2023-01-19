beed4e60858b6ee829391de8aabbf9e74bf0b30e
bfuzzy1 <54591490+bfuzzy1@users.noreply.github.com>
2023-01-19 07:21:20 -0500

    Update playwright.py

--- tests/61da9575-5c7e-471a-a3d7-27db8c7eea38/playwright.py
+++ tests/61da9575-5c7e-471a-a3d7-27db8c7eea38/playwright.py
@@ -1,130 +1,74 @@
-import playwright.sync_api as sync_api
-import time
-import os, binascii
-import sys
+import os
+import asyncio
+from playwright.async_api import Playwright, async_playwright, expect
 
 
-def login(page):
-    print("Logging in")
-    url = "https://github.com/login"
-    page.bring_to_front()
-    page.goto(url)
+async def main():
+    async with async_playwright() as p:
+        for browser_type in [p.chromium, p.firefox, p.webkit]:
+            browser = await browser_type.launch(headless=False)
+            page = await browser.new_page()
+            await page.goto("https://github.com/")
+            await page.get_by_role("link", name="Sign in").click()
 
-    # Wait for the login form to be present
-    page.wait_for_selector('#login_field')
+            username = os.environ.get('GITHUB_USERNAME')
+            password = os.environ.get('GITHUB_PASSWORD')
+            repo_name = os.environ.get('GITHUB_REPO_NAME')
 
-    username = os.environ.get('GITHUB_USERNAME')
-    password = os.environ.get('GITHUB_PASSWORD')
+            await page.get_by_label("Username or email address").click()
+            await page.get_by_label("Username or email address").fill(username)
+            await page.get_by_label("Password").click()
+            await page.get_by_label("Password").fill(password)
+            await page.get_by_role("button", name="Sign in").click()
+            await page.get_by_role("heading", name="Top Repositories New").get_by_role("link", name="New").click()
+            await page.get_by_label("Repository name").click()
+            await page.get_by_label("Repository name").fill(repo_name)
+            await page.get_by_label("Add a README file").check()
+            await page.get_by_role("button", name="Create repository").click()
+            await page.get_by_role("button", name="Add file").click()
+            await page.get_by_role("button", name="Create new file").click()
+            await page.locator("#code-editor pre").click()
+            await page.get_by_placeholder("Create hello_world.py").click()
+            await page.get_by_placeholder("Create hello_world.py").fill("adding hello world python script")
+            await page.get_by_role("button", name="Commit new file").click()
+            await page.get_by_role("link", name="Issues Issues").click()
+            await page.get_by_role("button", name="New issue").click()
+            await page.get_by_placeholder("Title").fill("Add goodbye to script")
+            await page.get_by_placeholder("Leave a comment").click()
+            await page.get_by_placeholder("Leave a comment").fill("This script is too basic and should allow for a user to select hello and print hello world and also the same thing for goodbye. This way the user has two options that do two different things.")
+            await page.get_by_role("button", name="Submit new issue").click()
+            await page.get_by_role("link", name="Actions Actions").click()
+            await page.get_by_role("link", name="set up a workflow yourself").click()
+            await page.get_by_placeholder("Name your file…").click()
+            await page.get_by_placeholder("Name your file…").click()
+            await page.get_by_placeholder("Name your file…").fill("hello.yaml")
+            await page.locator("#code-editor pre").click()
+            await page.locator("#code-editor").press("Enter")
+            await page.get_by_text("1name: hello2").press("Enter")
+            await page.get_by_text("1name: hello2on: push3").press("Enter")
+            await page.get_by_text("1name: hello2on: push3jobs:4").press("Tab")
+            await page.get_by_text("1name: hello2on: push3jobs:4").press("Enter")
+            await page.get_by_text("1name: hello2on: push3jobs:4 my-job:5").press("Tab")
+            await page.get_by_text("1name: hello2on: push3jobs:4 my-job:5").press("Enter")
+            await page.get_by_text("1name: hello2on: push3jobs:4 my-job:5 runs-on: ubuntu-latest6").press("Enter")
+            await page.get_by_text("1name: hello2on: push3jobs:4 my-job:5 runs-on: ubuntu-latest6 steps:7").press("Tab")
+            await page.get_by_text("1name: hello2on: push3jobs:4 my-job:5 runs-on: ubuntu-latest6 steps:7").press("Enter")
+            await page.get_by_text("1name: hello2on: push3jobs:4 my-job:5 runs-on: ubuntu-latest6 steps:7 - name: my").press("Tab")
+            await page.get_by_text("Start commit").click()
+            await page.get_by_placeholder("Create hello.yaml").click()
+            await page.get_by_placeholder("Create hello.yaml").fill("adding github action")
+            await page.get_by_role("button", name="Commit new file").click()
+            await page.get_by_role("link", name="Settings Settings").click()
+            await page.get_by_role("link", name="Branches").click()
+            await page.get_by_role("link", name="Add branch protection rule").click()
+            await page.get_by_label("Lock branch").check()
+            await page.get_by_label("Branch name pattern").click()
+            await page.get_by_label("Branch name pattern").fill("main")
+            await page.get_by_text("Create").click()
+            await page.get_by_role("link", name="Code Code").click()
+            await page.get_by_role("button", name="View profile and more").click()
+            await page.get_by_role("menuitem", name="Sign out").click()
+            await context.close()
+            await browser.close()
 
-    # Fill out the login form with your username and password
-    page.fill('#login_field', 'username')
-    page.fill('#password', 'password')
-
-    # Click the "Sign in" button
-    page.click('[type="submit"]')
-
-
-def changepassword(page):
-    print("Changing password of current user")
-    oldpassword = os.environ.get('GITHUB_PASSWORD')
-    newpassword = "t3sting1234554321@"
-
-    page.get_by_role("menuitem", name="Settings").click()
-    page.get_by_role("link", name="Password and authentication").click()
-    page.get_by_label("Old password").click()
-    page.get_by_label("Old password").fill(oldpassword)
-    page.locator("#user_new_password").click()
-    page.locator("#user_new_password").fill(newpassword)
-    page.get_by_label("Confirm new password").click()
-    page.get_by_label("Confirm new password").fill(newpassword)
-    page.get_by_role("button", name="Update password").click()
-
-
-def edit_repository(page):
-    page.get_by_role("button", name="View profile and more").click()
-    page.get_by_role("menuitem", name="Your repositories").click()
-    page.wait_for_load_state("networkidle")
-    page.get_by_role("link", name="Test-repo").click()
-    page.get_by_role("button", name="Add file").click()
-    page.get_by_role("button", name="Create new file").click()
-    page.get_by_placeholder("Name your file…").click()
-    page.get_by_placeholder("Name your file…").fill("Hello_World.py")
-    page.locator("#code-editor pre").click()
-    page.fill("#code-editor pre", "def hello_world():\n    print('hello world')\nhello_world()")
-    page.get_by_placeholder("Create Hello_World.py").click()
-    page.get_by_placeholder("Create Hello_World.py").fill("Testing Playwright")
-    page.get_by_role("button", name="Commit new file").click()
-
-
-def setup_github_test_action(page, repo_name):
-    repo_name = os.environ.get('GITHUB_REPO_NAME')
-    username = os.environ.get('GITHUB_USERNAME')
-
-    url = f"https://github.com/{username}/{repo_name}"
-    page.goto(url)
-    page.click('#actions-tab')
-    time.sleep(5)
-    # Click on the "Set up a workflow yourself" url
-    page.click('a[data-hydro-click*="actions.onboarding_setup_workflow_click"]')
-    time.sleep(5)
-
-    # Define the test GitHub action code
-    action_code = """
-    name: Test
-    on: [push]
-    jobs:
-    test:
-    runs-on: ubuntu-latest
-    steps:
-    - uses: actions/checkout@v2
-    - name: Test
-    run: echo 'Hello, world!'
-    """
-
-    page.wait_for_selector('#code-editor')
-    page.fill('#code-editor', action_code)
-    time.sleep(5)
-    page.click('summary[class*="btn-primary"]')
-    time.sleep(5)
-    page.wait_for_selector('#submit-file')
-    page.click('#submit-file')
-
-
-def select_browser(playwright):
-    browsers = ["chrome", "msedge", "firefox"]
-    for b in browsers:
-        try:
-            browser = playwright.chromium.launch(channel=b)
-            return browser
-        except:
-            continue
-    print("A supported browser is not available.")
-    sys.exit(1)
-
-
-def main():
-    print("Initializing playwright")
-    with sync_api.sync_playwright() as playwright:
-        browser = select_browser(playwright)
-    page = browser.new_page()
-    login(page)
-    # Check if login was successful
-    if page.url == 'https://github.com/':
-        print('Login successful!')
-        # Github test actions
-        setup_github_test_action(page)
-        time.sleep(10)
-        edit_repository(page)
-        time.sleep(10)
-        changepassword(page)
-        time.sleep(10)
-        sys.exit(100)
-    else:
-        # Login was not successful
-        print('Login failed!')
-        sys.exit(101)
-
-
-if __name__ == "__main__":
-    print("Starting engine")
-    main()
+asyncio.run(main())
