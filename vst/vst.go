8f431790eadfecb6bfdb1a584bce3ce0524564c2
david <coldduck@protonmail.com>
2023-01-23 08:15:36 -0500

    support multi workspaces

--- vst/vst.go
+++ vst/vst.go
@@ -10,13 +10,15 @@ type fn func()
 func Start(test fn, clean fn) {
 	args := os.Args[1:]
 	if len(args) > 0 {
+		println("Starting cleanup")
 		clean()
 	} else {
+		println("Starting test")
 		test()
 	}
 }
 
 func Stop(code int) {
-	print(fmt.Sprintf("Completed with code: %d", code))
+	println(fmt.Sprintf("Completed with code: %d", code))
 	os.Exit(code)
 }
