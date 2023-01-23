9e7c63424b88a2014be6555593f491cf1f207b23
david <coldduck@protonmail.com>
2023-01-23 07:28:39 -0500

    new mod for test management

---
+++ test/go.mod
@@ -0,0 +1,3 @@
+module github.com/preludeorg/test/test
+
+go 1.19
---
+++ test/test.go
@@ -0,0 +1,11 @@
+package Test
+
+import (
+	"fmt"
+	"os"
+)
+
+func Stop(code int, stage string) {
+	print(fmt.Sprintf("Completed %s stage: %d", stage, code))
+	os.Exit(code)
+}
