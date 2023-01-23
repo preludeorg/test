bd084b0568e9c25d225bb0792253a5d0d14da69b
gerbsec <albabawaseem@gmail.com>
2023-01-23 12:56:36 -0500

    windows port

--- tests/021a1dbf-e080-495c-937e-7fa79d39bf30/021a1dbf-e080-495c-937e-7fa79d39bf30.go
+++ tests/021a1dbf-e080-495c-937e-7fa79d39bf30/021a1dbf-e080-495c-937e-7fa79d39bf30.go
@@ -6,42 +6,42 @@ CREATED: 2023-01-21
 package main
 
 import (
-	"github.com/preludeorg/test/endpoint"
-	"runtime"
+    "github.com/preludeorg/test/endpoint"
+    "runtime"
 )
 
 func command1() string {
-	if runtime.GOOS == "windows" {
-		return "(([xml](Get-GPOReport -Name 'Default Domain Policy' -ReportType Xml)).GPO.Computer.ExtensionData.Extension.Account)| Select-Object Name, SettingNumber"
-	} else if runtime.GOOS == "darwin" {
-		return "pwpolicy getaccountpolicies"
-	} else {
-		return "cat /etc/pam.d/common-password"
-	}
+    if runtime.GOOS == "windows" {
+        return "(([xml](Get-GPOReport -Name 'Default Domain Policy' -ReportType Xml)).GPO.Computer.ExtensionData.Extension.Account)| Select-Object Name, SettingNumber"
+    } else if runtime.GOOS == "darwin" {
+        return "pwpolicy getaccountpolicies"
+    } else {
+        return "cat /etc/pam.d/common-password"
+    }
 }
 
 func command2() string {
-	if runtime.GOOS == "windows" {
-		return ""
-	} else {
-		return "awk '$1 ~ /^P.{20}$/ {print}' ~"
-	}
+    if runtime.GOOS == "windows" {
+        return "Get-ChildItem C:/ -File -Recurse | Select-String -List -Pattern '^P.{20}$' | Select-Object -ExpandProperty Path"
+    } else {
+        return "awk '$1 ~ /^P.{20}$/ {print}' ~"
+    }
 }
 
 func test() {
-	response1 := Endpoint.Run(command1())
-	print(response1)
-	if len(response1) > 0 {
-		response2 := Endpoint.Run(command2())
-		print(response2)
-	}
-	Endpoint.Stop(101)
+    response1 := Endpoint.Run(command1())
+    print(response1)
+    if len(response1) > 0 {
+        response2 := Endpoint.Run(command2())
+        print(response2)
+    }
+    Endpoint.Stop(101)
 }
 
 func clean() {
-	Endpoint.Stop(100)
+    Endpoint.Stop(100)
 }
 
 func main() {
-	Endpoint.Start(test, clean)
+    Endpoint.Start(test, clean)
 }
