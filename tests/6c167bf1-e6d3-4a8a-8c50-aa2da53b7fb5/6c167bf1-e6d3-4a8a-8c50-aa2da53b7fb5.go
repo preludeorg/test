a0475ebeed706b96191dc17e21b1c5d532d368df
gerbsec <albabawaseem@gmail.com>
2023-01-24 12:01:28 -0500

    Ghostpack

--- tests/6c167bf1-e6d3-4a8a-8c50-aa2da53b7fb5/6c167bf1-e6d3-4a8a-8c50-aa2da53b7fb5.go
+++ tests/6c167bf1-e6d3-4a8a-8c50-aa2da53b7fb5/6c167bf1-e6d3-4a8a-8c50-aa2da53b7fb5.go
@@ -1,6 +1,6 @@
 /*
-FILENAME: 6c167bf1-e6d3-4a8a-8c50-aa2da53b7fb5.go
-RULE: Ghostpack binaries should not run
+ID: 6c167bf1-e6d3-4a8a-8c50-aa2da53b7fb5
+RULE: Quarantine malicious files
 CREATED: 2023-01-22 11:07:25.006000
 */
 package main
@@ -8,58 +8,30 @@ package main
 import (
 	_ "embed"
 	"os"
-	"os/exec"
-	"runtime"
+	"path/filepath"
 
 	Endpoint "github.com/preludeorg/test/endpoint"
 )
 
-//go:embed Sharpwmi.exe
-var seatbelt []byte
-
-func joinPath(path string, file string) string {
-	if runtime.GOOS == "windows" {
-		return path + "\\" + file
-	} else {
-		return path + file
-	}
-}
-
-var binPath = joinPath(os.TempDir(), "Seatbelt.exe")
-
-func run(command string, args []string) (int, string, string) {
-	cmd := exec.Command(command, args...)
-	stdout, err := cmd.Output()
-	if err != nil {
-		if exitError, ok := err.(*exec.ExitError); ok {
-			return exitError.ExitCode(), string(stdout), string(exitError.Stderr)
-		} else {
-			return 1, string(stdout), err.Error()
-		}
-	}
-	return 0, string(stdout), ""
-}
+//go:embed SharpWMI.exe
+var sharpwmi []byte
+var binPath = filepath.Join(os.TempDir(), "SharpWMI.exe")
 
 func test() {
-	Endpoint.Write(binPath, seatbelt)
-	exitCode, _, _ := run("cmd.exe", []string{"/C", binPath})
+	Endpoint.Write(binPath, sharpwmi)
+	exitCode, _, _ := Endpoint.Run("cmd.exe", []string{"/C", binPath})
 	if exitCode == 0 {
-		os.Exit(101)
+		Endpoint.Stop(101)
 	} else {
-		os.Exit(100)
+		exitCode := Endpoint.Remove(binPath)
+		Endpoint.Stop(exitCode)
 	}
 }
 
 func clean() {
-	exitCode := Endpoint.Remove(binPath)
-	os.Exit(exitCode)
+	Endpoint.Stop(100)
 }
 
 func main() {
-	args := os.Args[1:]
-	if len(args) > 0 {
-		clean()
-	} else {
-		test()
-	}
+	Endpoint.Start(test, clean)
 }
--- tests/87f45943-055d-4bb1-9ef5-a1497f277935/87f45943-055d-4bb1-9ef5-a1497f277935.go
+++ tests/87f45943-055d-4bb1-9ef5-a1497f277935/87f45943-055d-4bb1-9ef5-a1497f277935.go
@@ -1,64 +1,37 @@
 /*
-FILENAME: 87f45943-055d-4bb1-9ef5-a1497f277935.go
-RULE: Ghostpack binaries should not run
+ID: 87f45943-055d-4bb1-9ef5-a1497f277935
+RULE: Quarantine malicious files
 CREATED: 2023-01-22 10:43:39.988000
 */
 package main
 
 import (
 	_ "embed"
-	Endpoint "github.com/preludeorg/test/endpoint"
 	"os"
-	"os/exec"
-	"runtime"
+	"path/filepath"
+
+	Endpoint "github.com/preludeorg/test/endpoint"
 )
 
 //go:embed Seatbelt.exe
 var seatbelt []byte
-
-func joinPath(path string, file string) string {
-	if runtime.GOOS == "windows" {
-		return path + "\\" + file
-	} else {
-		return path + file
-	}
-}
-
-var binPath = joinPath(os.TempDir(), "Seatbelt.exe")
-
-func run(command string, args []string) (int, string, string) {
-	cmd := exec.Command(command, args...)
-	stdout, err := cmd.Output()
-	if err != nil {
-		if exitError, ok := err.(*exec.ExitError); ok {
-			return exitError.ExitCode(), string(stdout), string(exitError.Stderr)
-		} else {
-			return 1, string(stdout), err.Error()
-		}
-	}
-	return 0, string(stdout), ""
-}
+var binPath = filepath.Join(os.TempDir(), "Seatbelt.exe")
 
 func test() {
 	Endpoint.Write(binPath, seatbelt)
-	exitCode, _, _ := run("cmd.exe", []string{"/C", binPath})
+	exitCode, _, _ := Endpoint.Run("cmd.exe", []string{"/C", binPath})
 	if exitCode == 0 {
-		os.Exit(101)
+		Endpoint.Stop(101)
 	} else {
-		os.Exit(100)
+		exitCode := Endpoint.Remove(binPath)
+		Endpoint.Stop(exitCode)
 	}
 }
 
 func clean() {
-	exitCode := Endpoint.Remove(binPath)
-	os.Exit(exitCode)
+	Endpoint.Stop(100)
 }
 
 func main() {
-	args := os.Args[1:]
-	if len(args) > 0 {
-		clean()
-	} else {
-		test()
-	}
+	Endpoint.Start(test, clean)
 }
