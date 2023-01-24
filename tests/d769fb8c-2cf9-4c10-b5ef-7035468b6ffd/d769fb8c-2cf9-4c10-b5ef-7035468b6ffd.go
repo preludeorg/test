5fd00373d6ff8fda56f4f8f56fa63eb641d10aad
david <coldduck@protonmail.com>
2023-01-24 07:09:46 -0500

    new network test

---
+++ tests/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd.go
@@ -0,0 +1,26 @@
+/*
+ID: d769fb8c-2cf9-4c10-b5ef-7035468b6ffd
+RULE: Block outbound network requests to specific countries
+CREATED: 2023-01-24
+*/
+package main
+
+import (
+	"github.com/preludeorg/test/endpoint"
+)
+
+func test() {
+	code := Endpoint.DialTCP("77.88.55.80:443", "hello world")
+	if code == 0 {
+		Endpoint.Stop(101)
+	}
+	Endpoint.Stop(100)
+}
+
+func clean() {
+	Endpoint.Stop(100)
+}
+
+func main() {
+	Endpoint.Start(test, clean)
+}
