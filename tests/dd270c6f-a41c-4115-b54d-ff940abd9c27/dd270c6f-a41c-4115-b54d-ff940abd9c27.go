1a8b967a3bd8590a9769a9265f4e1986e9c5bd91
VVX7 <info@vvx7.io>
2023-01-25 15:47:34 -0500

    refactor for new endpoint.run func. add explicit exit codes to shell commands.

--- tests/dd270c6f-a41c-4115-b54d-ff940abd9c27/dd270c6f-a41c-4115-b54d-ff940abd9c27.go
+++ tests/dd270c6f-a41c-4115-b54d-ff940abd9c27/dd270c6f-a41c-4115-b54d-ff940abd9c27.go
@@ -6,22 +6,32 @@ CREATED: 2023-01-21
 package main
 
 import (
-	"github.com/preludeorg/test/endpoint"
+	Endpoint "github.com/preludeorg/test/endpoint"
 	"runtime"
 )
 
-func command() string {
+func command() (string, []string) {
+	var command string
+	var args []string
 	if runtime.GOOS == "windows" {
-		return "Invoke-RestMethod -UseBasicParsing -Uri ('http://ipinfo.io/'+ (Invoke-WebRequest -UseBasicParsing -uri 'http://ifconfig.me/ip').Content)"
+		command = "powershell.exe"
+		args = []string{"try { Invoke-RestMethod -UseBasicParsing -Uri ('http://ipinfo.io/'+ (Invoke-WebRequest -UseBasicParsing -uri 'http://ifconfig.me/ip').Content); exit 101 } catch { exit 100 }"}
 	} else {
-		return "wget -qO- http://ifconfig.me/ip | wget -qO- http://ipinfo.io/$1"
+		command = "bash"
+		args = []string{"-c", "if wget -qO- http://ifconfig.me/ip | wget -qO- http://ipinfo.io/$1 ; then exit 101 ; else exit 100 ; fi"}
 	}
+	return command, args
 }
 
 func test() {
-	response := Endpoint.Run(command())
-	print(response)
-	Endpoint.Stop(101)
+	command, args := command()
+	exitCode, stdout, stderr := Endpoint.Run(command, args)
+	if exitCode != 0 {
+		println(stderr)
+		Endpoint.Stop(exitCode)
+	}
+	println(stdout)
+	Endpoint.Stop(exitCode)
 }
 
 func clean() {
