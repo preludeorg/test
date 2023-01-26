d6e1d0cb4ddc9cadb4d3ad439e8f228c21f791b4
VVX7 <info@vvx7.io>
2023-01-26 15:16:13 -0500

    refactor 021a1dbf-e080-495c-937e-7fa79d39bf30

--- tests/021a1dbf-e080-495c-937e-7fa79d39bf30/021a1dbf-e080-495c-937e-7fa79d39bf30.go
+++ tests/021a1dbf-e080-495c-937e-7fa79d39bf30/021a1dbf-e080-495c-937e-7fa79d39bf30.go
@@ -6,36 +6,51 @@ CREATED: 2023-01-21
 package main
 
 import (
-	"github.com/preludeorg/test/endpoint"
+	Endpoint "github.com/preludeorg/test/endpoint"
 	"runtime"
 )
 
-func command1() string {
+func executor() string {
 	if runtime.GOOS == "windows" {
-		return "(([xml](Get-GPOReport -Name 'Default Domain Policy' -ReportType Xml)).GPO.Computer.ExtensionData.Extension.Account)| Select-Object Name, SettingNumber"
-	} else if runtime.GOOS == "darwin" {
-		return "pwpolicy getaccountpolicies"
+		return "powershell.exe"
 	} else {
-		return "cat /etc/pam.d/common-password"
+		return "bash"
 	}
 }
 
-func command2() string {
+func command1() []string {
 	if runtime.GOOS == "windows" {
-		return "Get-ChildItem C:/ -File -Recurse | Select-String -List -Pattern '^P.{20}$' | Select-Object -ExpandProperty Path"
+		return []string{"(([xml](Get-GPOReport -Name 'Default Domain Policy' -ReportType Xml)).GPO.Computer.ExtensionData.Extension.Account)| Select-Object Name, SettingNumber"}
+	} else if runtime.GOOS == "darwin" {
+		return []string{"-c", "pwpolicy getaccountpolicies"}
 	} else {
-		return "awk '$1 ~ /^P.{20}$/ {print}' ~"
+		return []string{"-c", "cat /etc/pam.d/common-password"}
+	}
+}
+
+func command2(stdout string) []string {
+	if runtime.GOOS == "windows" {
+		return []string{"Get-ChildItem C:/ -File -Recurse | Select-String -List -Pattern '^P.{20}$' | Select-Object -ExpandProperty Path"}
+	} else {
+		return []string{"-c", "cat << EOF | awk '$1 ~ /^P.{20}$/ {print}'\n" + stdout + "\nEOF"}
 	}
 }
 
 func test() {
-	response1 := Endpoint.Run(command1())
-	print(response1)
-	if len(response1) > 0 {
-		response2 := Endpoint.Run(command2())
-		print(response2)
+	exitCode, stdout, stderr := Endpoint.Run(executor(), command1())
+	if exitCode != 0 {
+		println(stderr)
+		Endpoint.Stop(exitCode)
 	}
-	Endpoint.Stop(101)
+	println(stdout)
+
+	exitCode2, stdout2, stderr2 := Endpoint.Run(executor(), command2(stdout))
+	if exitCode2 != 0 {
+		println(stderr2)
+		Endpoint.Stop(exitCode)
+	}
+	println(stdout2)
+	Endpoint.Stop(100)
 }
 
 func clean() {
