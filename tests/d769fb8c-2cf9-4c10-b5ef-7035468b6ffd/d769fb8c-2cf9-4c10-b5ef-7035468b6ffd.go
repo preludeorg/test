a68e096ec9fc1104ec7221987d87ecc198ec7597
gerbsec <albabawaseem@gmail.com>
2023-01-31 16:33:49 -0500

    china and russia write loop

--- tests/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd.go
+++ tests/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd.go
@@ -1,20 +1,29 @@
 /*
 ID: d769fb8c-2cf9-4c10-b5ef-7035468b6ffd
-NAME: Enforce an outbound firewall
-CREATED: 2023-01-24
+RULE: Enforce an outbound firewall
+CREATED: 2023-01-31 07:59:52.294747
 */
 package main
 
 import (
-	"github.com/preludeorg/test/endpoint"
+	Endpoint "github.com/preludeorg/test/endpoint"
 )
 
+var ips = []string{
+	"77.88.55.80:443",
+	"39.156.66.10:443",
+}
+
 func test() {
-	code := Endpoint.DialTCP("77.88.55.80:443", "hello world")
-	if code == 0 {
-		Endpoint.Stop(101)
+	for _, ip := range ips {
+		code := Endpoint.DialTCP(ip, "hello world")
+		if code == 0 {
+			println("[+] Message sent successfully to", ip)
+			continue
+		}
+		println("[-] Failed to send message to", ip)
 	}
-	Endpoint.Stop(100)
+	Endpoint.Stop(101)
 }
 
 func clean() {
@@ -24,3 +33,4 @@ func clean() {
 func main() {
 	Endpoint.Start(test, clean)
 }
+
