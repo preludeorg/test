91cf4801c8dc17c307f882a728fd073b1058e32e
bfuzzy1 <54591490+bfuzzy1@users.noreply.github.com>
2023-01-31 16:59:54 -0500

    stuff changes

--- tests/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd.go
+++ tests/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd.go
@@ -1,6 +1,6 @@
 /*
 ID: d769fb8c-2cf9-4c10-b5ef-7035468b6ffd
-RULE: Enforce an outbound firewall
+NAME: Enforce an outbound firewall
 CREATED: 2023-01-31 07:59:52.294747
 */
 package main
@@ -10,8 +10,8 @@ import (
 )
 
 var ips = []string{
-	"77.88.55.80:443",
-	"39.156.66.10:443",
+	"77.88.55.80:443", // https://yandex.com
+	"104.193.88.77:443", // https://www.baidu.cn
 }
 
 func test() {
@@ -23,7 +23,7 @@ func test() {
 		}
 		println("[-] Failed to send message to", ip)
 	}
-	Endpoint.Stop(101)
+	Endpoint.Stop(106)
 }
 
 func clean() {
