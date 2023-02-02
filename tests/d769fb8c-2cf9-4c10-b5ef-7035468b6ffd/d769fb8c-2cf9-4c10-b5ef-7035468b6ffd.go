f75ff957ac5609139d6a53c0e5f08540a8287591
gerbsec <67713732+gerbsec@users.noreply.github.com>
2023-02-02 11:35:14 -0500

    china and russia write loop (#56)
    * china and russia write loop
    
    * stuff changes
    
    * modify tcpdial func
    
    * revert tcpdial change
    
    * firewall
    
    * else for logic
    
    * Update README.md
    
    ---------
    
    Co-authored-by: bfuzzy1 <54591490+bfuzzy1@users.noreply.github.com>
    Co-authored-by: VVX7 <info@vvx7.io>

--- tests/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd.go
+++ tests/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd/d769fb8c-2cf9-4c10-b5ef-7035468b6ffd.go
@@ -1,20 +1,31 @@
 /*
 ID: d769fb8c-2cf9-4c10-b5ef-7035468b6ffd
 NAME: Enforce an outbound firewall
-CREATED: 2023-01-24
+CREATED: 2023-01-31 07:59:52.294747
 */
 package main
 
 import (
-	"github.com/preludeorg/test/endpoint"
+	Endpoint "github.com/preludeorg/test/endpoint"
 )
 
+var ips = []string{
+	"77.88.55.80:443",   // https://yandex.com
+	"104.193.88.77:443", // https://www.baidu.cn
+}
+
 func test() {
-	code := Endpoint.DialTCP("77.88.55.80:443", "hello world")
-	if code == 0 {
-		Endpoint.Stop(101)
+	exit := 106
+	for _, ip := range ips {
+		code := Endpoint.NetworkTest(ip, "hello world")
+		if code == 0 {
+			println("[+] Message sent successfully to ", ip)
+			exit = 101
+		} else {
+			println("[-] Failed to send message to ", ip)
+		}
 	}
-	Endpoint.Stop(100)
+	Endpoint.Stop(exit)
 }
 
 func clean() {
