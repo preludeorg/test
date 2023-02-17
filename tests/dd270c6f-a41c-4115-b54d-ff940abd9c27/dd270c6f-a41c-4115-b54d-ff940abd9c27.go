0763d58b5ed06f0319544745bc52b4b80b67e435
gerbsec <albabawaseem@gmail.com>
2023-02-17 10:44:54 -0500

    remove ping

--- tests/dd270c6f-a41c-4115-b54d-ff940abd9c27/dd270c6f-a41c-4115-b54d-ff940abd9c27.go
+++ tests/dd270c6f-a41c-4115-b54d-ff940abd9c27/dd270c6f-a41c-4115-b54d-ff940abd9c27.go
@@ -9,9 +9,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net"
 	"net/http"
-	"os/exec"
 	"strings"
 
 	Endpoint "github.com/preludeorg/test/endpoint"
@@ -64,49 +62,6 @@ func searchShodan(ip string) {
 	}
 }
 
-func pingSweep() {
-	interfaces, err := net.Interfaces()
-	if err != nil {
-		println(err)
-		return
-	}
-
-	for _, i := range interfaces {
-		if i.Flags&net.FlagUp == 0 {
-			continue
-		}
-		if i.Flags&net.FlagLoopback != 0 {
-			continue
-		}
-		addrs, err := i.Addrs()
-		if err != nil {
-			println(err)
-			return
-		}
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-			if ip == nil || ip.IsLoopback() {
-				continue
-			}
-			if ip.To4() != nil {
-				ipStr := ip.String()
-				cmd := exec.Command("ping", "-c", "1", "-t", "1", ipStr)
-				if err := cmd.Run(); err != nil {
-					println(ipStr, "is down")
-				} else {
-					println(ipStr, "is up and belongs to", i.HardwareAddr.String())
-				}
-			}
-		}
-	}
-}
-
 func test() {
 	println("[+] Getting Public IP Address & Location")
 	query, lat, long, err := getPublicIP()
@@ -121,9 +76,6 @@ func test() {
 
 	println("[+] Searching IP on Shodan")
 	searchShodan(query)
-
-	println("[+] Ping Sweep")
-	pingSweep()
 
 	Endpoint.Stop(101)
 }
