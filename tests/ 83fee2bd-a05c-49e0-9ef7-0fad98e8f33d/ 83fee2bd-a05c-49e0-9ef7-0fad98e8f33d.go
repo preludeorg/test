0f2b0541e46f29ce7a4d0b10f8d3a1e56de8550e
gerbsec <albabawaseem@gmail.com>
2023-02-23 10:09:52 -0500

    arpscan

---
+++ tests/ 83fee2bd-a05c-49e0-9ef7-0fad98e8f33d/ 83fee2bd-a05c-49e0-9ef7-0fad98e8f33d.go
@@ -0,0 +1,160 @@
+/*
+ID: 83fee2bd-a05c-49e0-9ef7-0fad98e8f33d
+NAME: name this for me robin lool (arpscan)
+CREATED: 2023-02-23 10:08:34.720000
+*/
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"log"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	Endpoint "github.com/preludeorg/test/endpoint"
+)
+
+func scan(iface *net.Interface) error {
+	var addr *net.IPNet
+	if addrs, err := iface.Addrs(); err != nil {
+		return err
+	} else {
+		for _, a := range addrs {
+			if ipnet, ok := a.(*net.IPNet); ok {
+				if ip4 := ipnet.IP.To4(); ip4 != nil {
+					addr = &net.IPNet{
+						IP:   ip4,
+						Mask: ipnet.Mask[len(ipnet.Mask)-4:],
+					}
+					break
+				}
+			}
+		}
+	}
+	if addr == nil {
+		return nil
+	} else if addr.IP[0] == 127 {
+		return nil
+	} else if addr.Mask[0] != 0xff || addr.Mask[1] != 0xff {
+		return errors.New("mask means network is too large")
+	}
+	log.Printf("Using network range %v for interface %v", addr, iface.Name)
+
+	handle, err := pcap.OpenLive(iface.Name, 65536, true, pcap.BlockForever)
+	if err != nil {
+		return err
+	}
+	defer handle.Close()
+
+	stop := make(chan struct{})
+	go readARP(handle, iface, stop)
+	defer close(stop)
+	for {
+		if err := writeARP(handle, iface, addr); err != nil {
+			log.Printf("error writing packets on %v: %v", iface.Name, err)
+			return err
+		}
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func readARP(handle *pcap.Handle, iface *net.Interface, stop chan struct{}) {
+	src := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet)
+	in := src.Packets()
+	for {
+		var packet gopacket.Packet
+		select {
+		case <-stop:
+			return
+		case packet = <-in:
+			arpLayer := packet.Layer(layers.LayerTypeARP)
+			if arpLayer == nil {
+				continue
+			}
+			arp := arpLayer.(*layers.ARP)
+			if arp.Operation != layers.ARPReply || bytes.Equal([]byte(iface.HardwareAddr), arp.SourceHwAddress) {
+				continue
+			}
+			log.Printf("IP %v is at %v", net.IP(arp.SourceProtAddress), net.HardwareAddr(arp.SourceHwAddress))
+
+		}
+	}
+}
+
+func writeARP(handle *pcap.Handle, iface *net.Interface, addr *net.IPNet) error {
+	eth := layers.Ethernet{
+		SrcMAC:       iface.HardwareAddr,
+		DstMAC:       net.HardwareAddr{0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+		EthernetType: layers.EthernetTypeARP,
+	}
+	arp := layers.ARP{
+		AddrType:          layers.LinkTypeEthernet,
+		Protocol:          layers.EthernetTypeIPv4,
+		HwAddressSize:     6,
+		ProtAddressSize:   4,
+		Operation:         layers.ARPRequest,
+		SourceHwAddress:   []byte(iface.HardwareAddr),
+		SourceProtAddress: []byte(addr.IP),
+		DstHwAddress:      []byte{0, 0, 0, 0, 0, 0},
+	}
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+	for _, ip := range ips(addr) {
+		arp.DstProtAddress = []byte(ip)
+		gopacket.SerializeLayers(buf, opts, &eth, &arp)
+		if err := handle.WritePacketData(buf.Bytes()); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func ips(n *net.IPNet) (out []net.IP) {
+	num := binary.BigEndian.Uint32([]byte(n.IP))
+	mask := binary.BigEndian.Uint32([]byte(n.Mask))
+	network := num & mask
+	broadcast := network | ^mask
+	for network++; network < broadcast; network++ {
+		var buf [4]byte
+		binary.BigEndian.PutUint32(buf[:], network)
+		out = append(out, net.IP(buf[:]))
+	}
+	return
+}
+
+func test() {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+
+	var wg sync.WaitGroup
+	for _, iface := range ifaces {
+		wg.Add(1)
+		go func(iface net.Interface) {
+			defer wg.Done()
+			if err := scan(&iface); err != nil {
+				log.Printf("interface %v: %v", iface.Name, err)
+			}
+		}(iface)
+	}
+	wg.Wait()
+	Endpoint.Stop(101)
+}
+
+func clean() {
+	Endpoint.Stop(100)
+}
+
+func main() {
+	Endpoint.Start(test, clean)
+}
