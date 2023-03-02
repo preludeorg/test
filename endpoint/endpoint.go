48a704d079a8a704c6b79c40fee683cef236822d
gerbsec <albabawaseem@gmail.com>
2023-03-02 11:00:10 -0500

    formatfix

--- endpoint/endpoint.go
+++ endpoint/endpoint.go
@@ -29,11 +29,11 @@ func Stop(code int) {
 }
 
 func RunWithTimeout(function fn) {
-    go function()
-    select {
-    case <- time.After(10 * time.Second):
-        os.Exit(102)
-    }
+	go function()
+	select {
+	case <-time.After(10 * time.Second):
+		os.Exit(102)
+	}
 }
 
 func Find(ext string) []string {
@@ -151,15 +151,11 @@ func Shell(args []string) (string, error) {
 }
 
 func IsAvailable(programs ...string) bool {
-	pathEnv := os.Getenv("PATH")
-	pathList := filepath.SplitList(pathEnv)
 
 	for _, program := range programs {
-		for _, dir := range pathList {
-			programPath := filepath.Join(dir, program)
-			if Exists(programPath) {
-				return true
-			}
+		_, err := exec.LookPath(program)
+		if err == nil {
+			return true
 		}
 	}
 	return false
