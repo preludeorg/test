083119919df24a9526dc86e951c30b7ec43ba269
bfuzzy1 <54591490+bfuzzy1@users.noreply.github.com>
2023-03-07 15:55:57 -0500

    tweaks

--- tests/74077d3b-6a2f-49fa-903e-99cad6f34cf6/74077d3b-6a2f-49fa-903e-99cad6f34cf6.go
+++ tests/74077d3b-6a2f-49fa-903e-99cad6f34cf6/74077d3b-6a2f-49fa-903e-99cad6f34cf6.go
@@ -1,6 +1,6 @@
 /*
 ID: 74077d3b-6a2f-49fa-903e-99cad6f34cf6
-NAME: Will your computer quarantine oRat malware?
+NAME: Will your computer quarantine oRAT malware?
 CREATED: 2023-02-28
 */
 package main
@@ -11,13 +11,13 @@ import (
 	Endpoint "github.com/preludeorg/test/endpoint"
 )
 
-//go:embed oRat
+//go:embed oRAT
 var malicious []byte
 
 func test() {
 	println("[+] Extracting file for quarantine test")
 	println("[+] Pausing for 1 second to gauge defensive reaction")
-	if Endpoint.Quarantined("oRat", malicious) {
+	if Endpoint.Quarantined("oRAT", malicious) {
 		println("[+] Malicious file was caught!")
 		Endpoint.Stop(105)
 	}
@@ -27,7 +27,7 @@ func test() {
 
 func clean() {
 	println("[+] Ensuring malicious file is no longer present")
-	if Endpoint.Remove("oRat") {
+	if Endpoint.Remove("oRAT") {
 		Endpoint.Stop(100)
 	}
 	Endpoint.Stop(105)
